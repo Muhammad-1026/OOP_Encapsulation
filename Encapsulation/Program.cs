7788b097e20b4793f3d4409739a669a2b661fdf9
Muhammad-1026 <bekbutaevmuhammad1026@gmail.com>
2025-07-19 13:05:58 +0500

    encapsulation

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,9 @@
+﻿using Encapsulation;
+
+Student student = new Student();
+
+student.SetName("John Doe");
+student.SetAge(20);
+
+Console.WriteLine($"Name: {student.GetName()}");
+Console.WriteLine($"Age: {student.GetAge()}");
---
+++ Encapsulation/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace Encapsulation;
+
+public class Student
+{
+    private string Name;
+    private int Age;
+
+    public string GetName()
+    {
+        return Name;
+    }
+
+    public int GetAge()
+    {
+        return Age;
+    }
+
+    public void SetName(string name)
+    {
+        Name = name;
+    }
+
+    public void SetAge(int age)
+    {
+        if (age < 0 || age >= 150)
+        {
+            throw new ArgumentException("Age is wrong");
+        }
+
+        Age = age;
+    }
+}
